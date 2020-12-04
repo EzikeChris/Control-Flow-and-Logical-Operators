93dbaa72d0737b54c03688ae0483f91fe3629702
christophermbe1009@gmail.com <christophermbe1009@gmail.com>
2020-12-04 10:45:01 +0100

    initial commit

---
+++ IfelseandConditionalOperators.py
@@ -0,0 +1,11 @@
+# Conditional statement #
+
+print("Welcome to Wonderland Abuja Nigeria !!")
+
+height = int(input("what is your height in cm?"))
+
+if height >= 120: # greater than means not equal to 120cm but if you want it to be a equal 120 and above 120 cm you add the equals sign
+    print("You can ride the rollercoaster!")
+else:
+    print("sorry, you have to grow taller before you can ride")
+
